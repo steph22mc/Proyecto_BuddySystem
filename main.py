568a9f5cce24427aad99a66d4517ca7fb60ccec0
steph22mc <127963741+steph22mc@users.noreply.github.com>
2023-10-13 23:41:57 -0500

    Este commit agrega las clases principales relacionadas con la gestión de memoria. Incluye las clases Memoria, Block, MemoryManager, Proceso y ProcessTreeNode.

---
+++ main.py
@@ -0,0 +1,17 @@
+# Importar Clases que se han creado
+from block import Block
+from memoria import Memoria
+from memory_manager import MemoryManager
+from proceso import Proceso
+from process_tree_node import ProcessTreeNode
+
+# Función Principal
+def main():
+    #Crear instancias de las clases
+
+    # Asignar memoria a procesos
+    # Imprimir el estado de la memoria
+    # ...
+    pass
+
+# Llamar a la Función
