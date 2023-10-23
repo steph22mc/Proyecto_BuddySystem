19ccdf9bef1eade1b6bb110f25436b096821aa66
Raul-Luna <147109745+Raul-Luna@users.noreply.github.com>
2023-10-23 14:46:24 -0500

    proceso.py

--- proceso.py
+++ proceso.py
@@ -1,2 +1,8 @@
 class Proceso:
+     def __init__(self, nombre, tamaño):
+        self.nombre = nombre  
+        self.tamaño = tamaño  
+
+    def __str__(self):
+        return f"Proceso: {self.nombre}, Tamaño: {self.tamaño} bytes"
     pass
