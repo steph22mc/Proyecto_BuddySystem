429ccec286a6e0016ecc210700ec5dc61d57740d
Raul-Luna <147109745+Raul-Luna@users.noreply.github.com>
2023-10-23 14:45:14 -0500

    block.py

--- block.py
+++ block.py
@@ -1,2 +1,18 @@
+#raul luna
 class Block:
-    pass
+    class Block:
+    def __init__(self, dirección, tamaño, ocupado):
+        self.dirección = dirección 
+        self.tamaño = tamaño  
+        self.ocupado = ocupado  
+
+    def marcar_ocupado(self):
+        self.ocupado = True
+
+    def marcar_libre(self):
+        self.ocupado = False
+
+    def __str__(self):
+        estado = "Ocupado" if self.ocupado else "Libre"
+        return f"Dirección: {self.dirección}, Tamaño: {self.tamaño}, Estado: {estado}"
+pass
